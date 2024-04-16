36a22d4fde8efe669b752b69d42d391aee63c465
Thedileep <92201588+Thedileep@users.noreply.github.com>
2024-04-16 16:10:39 +0000

    commit successfully

--- auth/tut/src/App.js
+++ auth/tut/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React from 'react';
+  import { useState } from 'react';
 import './App.css';
 import Login from './components/Login'; 
 import SignUp from './components/Signup';
